adc6b16371a611e9e2253a04c49327b53b6491f8
razzemans <bas@q42.nl>
2020-02-04 11:57:03 +0100

    poc project

---
+++ LoggerCrash/LoggerCrash/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LoggerCrash.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
